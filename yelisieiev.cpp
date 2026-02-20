bfb68ab10c7e0ae39197245be76ad895c22ffff6
max190708 <yelisieiev.maksym.studente@itispaleocapa.it>
2026-02-20 10:50:32 +0100

    Menu

--- yelisieiev.cpp
+++ yelisieiev.cpp
@@ -8,7 +8,7 @@ void visualizza (int prod[],int d )
 {
 	
 }
-void cerca(int prodotto,int prod[],int d)
+int cerca(int prodotto,int prod[],int d)
 {
 	
 }
@@ -21,10 +21,49 @@ void modifica(bool a, int prodotto,int prod[],int d)
 	
 }
 int main()
+
 {
 	// nome,prodotto,prezzo,categoria;
 	int prodotto;
 	int prod[]={};
 	int nome[]={};
+	int d;
+	int opzione;
+	bool a;
+	do{
+	cout<<"MENU :"<<endl;
+	cout<<"1 --- Aggiungi"<<endl;
+	cout<<"2 --- Visualizza"<<endl;
+	cout<<"3 --- Cerca"<<endl;
+	cout<<"4 --- Cancella"<<endl;
+	cout<<"5 --- Modifica"<<endl;
+	cout<<"0 --- Esci"<<endl;
+	cout<<"Scegli l'opzione :"<<endl;
+	cin>>opzione;
+	switch(opzione)
+	{
+	case 1:
+	aggiungi( prodotto, prod, d);
+	break;	
+	
+	case 2:
+    visualizza(prod, d);
+	break;	
+	
+	case 3:
+	cerca(prodotto, prod, d);
+	break;	
+	
+	case 4:
+	cancella(a,prodotto, prod, d);
+	break;	
+	
+	case 5:
+	modifica(a,prodotto, prod, d);
+	break;	
+	
+	}
+	}while(opzione!=0);
 }
-
+	
+	
