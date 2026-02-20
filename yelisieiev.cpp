1d65d6c9f40feaf6c97d90583c2b3d325c24fc54
max190708 <yelisieiev.maksym.studente@itispaleocapa.it>
2026-02-20 11:22:42 +0100

    Aggiunta 3

--- yelisieiev.cpp
+++ yelisieiev.cpp
@@ -1,76 +1,74 @@
-#include <iostream>
-using namespace std;
-void aggiungi(int prodotto,int prod[],int d)
-{
-int num,pos;
-	int num,pos=0;
-	cout<<"Inserisci il numero che vuoi aggiungere:"<<endl;
-	cin>>num;
-	prod[pos]=num;
-	pos++;
-}
-void visualizza (int prod[],int d )
-{
-	
-}
-int cerca(int prodotto,int prod[],int d)
-{
-return true;	
-}
-void cancella(bool a, int prodotto,int prod[],int d)
-{
-	
-}
-void modifica(bool a, int prodotto,int prod[],int d)
-{
-	
-}
-int main()
-
-{
-	// nome,prodotto,prezzo,categoria;
-	int prodotto;
-	int prod[]={};
-	int nome[]={};
-	int d;
-	int opzione;
-	bool a;
-	do{
-	cout<<"MENU :"<<endl;
-	cout<<"1 --- Aggiungi"<<endl;
-	cout<<"2 --- Visualizza"<<endl;
-	cout<<"3 --- Cerca"<<endl;
-	cout<<"4 --- Cancella"<<endl;
-	cout<<"5 --- Modifica"<<endl;
-	cout<<"0 --- Esci"<<endl;
-	cout<<"Scegli l'opzione :"<<endl;
-	cin>>opzione;
-	switch(opzione)
+	#include <iostream>
+	using namespace std;
+	void aggiungi(int prodotto,int prod[],int &d)
 	{
-	case 1:
-	aggiungi( prodotto, prod, d);
-	break;	
-	
-	case 2:
-    visualizza(prod, d);
-	break;	
-	
-	case 3:
-	cerca(prodotto, prod, d);
-	break;	
-	
-	case 4:
-	cancella(a,prodotto, prod, d);
-	break;	
-	
-	case 5:
-	modifica(a,prodotto, prod, d);
-	break;	
-	
+	int num;
+	cout<<"Inserisci il numero che vuoi aggiungere : "<<endl;
+	cin>>num;
+	prod[d]=num;
+	cout<<"Elemento aggiunto con successo.";
+	d++;
 	}
-	}while(opzione!=0);
-}
+	void visualizza (int prod[],int d )
+	{
+		
+	}
+	int cerca(int prodotto,int prod[],int d)
+	{
+	return true;	
+	}
+	void cancella(bool a, int prodotto,int prod[],int d)
+	{
+		
+	}
+	void modifica(bool a, int prodotto,int prod[],int d)
+	{
+		
+	}
+	int main()
 	
-	
-
-
+	{
+		// nome,prodotto,prezzo,categoria;
+		int prodotto;
+		int prod[]={};
+		int nome[]={};
+		int d;
+		int opzione;
+		bool a;
+		do{
+		cout<<"MENU :"<<endl;
+		cout<<"1 --- Aggiungi"<<endl;
+		cout<<"2 --- Visualizza"<<endl;
+		cout<<"3 --- Cerca"<<endl;
+		cout<<"4 --- Cancella"<<endl;
+		cout<<"5 --- Modifica"<<endl;
+		cout<<"0 --- Esci"<<endl;
+		cout<<"Scegli l'opzione :"<<endl;
+		cin>>opzione;
+		switch(opzione)
+		{
+		case 1:
+		aggiungi( prodotto, prod, d);
+		break;	
+		
+		case 2:
+	    visualizza(prod, d);
+		break;	
+		
+		case 3:
+		cerca(prodotto, prod, d);
+		break;	
+		
+		case 4:
+		cancella(a,prodotto, prod, d);
+		break;	
+		
+		case 5:
+		modifica(a,prodotto, prod, d);
+		break;	
+		
+		}
+		}while(opzione!=0);
+	}
+		
+		
