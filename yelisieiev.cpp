6c0a7859d868bad444f18882300a7bb8e3bc50ba
max190708 <yelisieiev.maksym.studente@itispaleocapa.it>
2026-03-03 08:58:27 +0100

    Update yelisieiev.cpp
    Codice finale.

--- yelisieiev.cpp
+++ yelisieiev.cpp
@@ -22,15 +22,47 @@
 	}
 	int cerca(int prodotto,int prod[],int d)
 	{
-	return true;	
+	for (int i=0;i<d;i++)
+    {
+        if (prod[i]=prodotto)
+        {
+            return i;  
+        }
+    }
+	return -1;	
 	}
 	void cancella(bool a, int prodotto,int prod[],int d)
 	{
+		cout<<"Inserisci il valore da cancellare: "<<endl;
+		cin>>prodotto;
+	    a=false;
+		for(int i=0;i<d;i++)
+		{
+		    if(prod[i]==prodotto)
+		    {
+		        prod[i]=0;
+		        cout<<"Elemento cancellato con successo."<<endl;
+		        a=true;
+		    }
+		     }
+		     if(a==false)
+		      cout<<"Elemento non trovato."<<endl;
 		
 	}
 	void modifica(bool a, int prodotto,int prod[],int d)
 	{
-		
+	    int pos,num;
+		cout<<"Inserisci la posizione che vuoi modificare : "<<endl;
+		cin>>pos;
+		if(pos>d+1)
+		cout<<"Posizione non valida . "<<endl;
+		else
+		{
+		cout<<"Inserisci il valore che vuoi inserire : "<<endl;
+		cin>>num;
+		prod[pos-1]=num;
+		cout<<"Elemento modificato . "<<endl;
+		}
 	}
 	int main()
 	
@@ -66,9 +98,16 @@
 		break;	
 		
 		case 3:
-		cerca( prodotto, prod, d);
-		break;
-		
+			{
+                cout<<"Inserisci il valore da cercare: ";
+                cin>>prodotto;
+                int pos=cerca( prodotto, prod, d);
+                if (pos==1)
+                    cout<<"Elemento non trovato."<<endl;
+                else
+                    cout<<"Elemento trovato in posizione: "<<pos+1<<endl;
+                break;
+			}
 		case 4:
 		cancella(a,prodotto, prod, d); 
 		break;	
@@ -78,7 +117,11 @@
 		break;	
 		
 		case 0:
+		cout<<"Uscita dal programma.";
 		return 0;
+		
+		default:
+		cout<<"Opzione non valida."<<endl;
 		}
 		}while(opzione!=0);
 	}
